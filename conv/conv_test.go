92c01d4aa553e55ac8ba69fc03553b7e2506e58d
Janis Gailis <janis.gailis@uia.no>
2023-03-06 21:17:06 +0100

    withTolerance

--- conv/conv_test.go
+++ conv/conv_test.go
@@ -3,6 +3,7 @@ package conv
 import (
 	"reflect"
 	"testing"
+        "math"
 	//"fmt"
 )
 
@@ -14,22 +15,44 @@ import (
 	funksjonsnavn og testverdier)
 	Korrekt skrivemåte: Fahrenheit
 */
+
+// Eksempel på bruken av withTolerance
+func withinTolerance(a, b, error float64) bool {
+  // Først sjekk om tallene er nøyaktig like
+  if a == b {
+    return true
+  }
+
+  difference := math.Abs(a - b)
+
+  // Siden vi skal dele med b, må vi sjekke om den er 0
+  // Hvis b er 0, returner avgjørelsen om d er mindre enn feilmarginen
+  // som vi aksepterer
+  if b == 0 {
+    return difference < error
+  }
+
+  // Tilslutt sjekk den relative differanse mot feilmargin
+  return (difference/math.Abs(b)) < error
+}
+
 // Celsius = (Farhrenheit - 32)*(5/9)
 func TestFahrenheitToCelsius(t *testing.T) {
-	type test struct {
-		input float64
-		want  float64
-	}
+        type test struct {
+                input float64
+                want  float64
+                error float64
+        }
 
-	tests := []test{
-		{input: 134, want: 56.66666666666667},
-	}
+        tests := []test{
+                {input: 134, want: 56.67, error: 1e-2},
+        }
 
 	for _, tc := range tests {
-		got := FahrenheitToCelsius(tc.input)
-		if !reflect.DeepEqual(tc.want, got) {
-			t.Errorf("expected: %v, got: %v", tc.want, got)
-		}
+  		got := FahrenheitToCelsius(tc.input)
+  		if !withinTolerance(tc.want, got, tc.error) {
+    			t.Errorf("expected: %.18f, got: %.18f", tc.want, got)
+  		}
 	}
 }
 
@@ -38,18 +61,28 @@ func TestCelsiusToFahrenheit(t *testing.T) {
 	type test struct {
 		input float64
 		want  float64
+		error float64
 	}
 
 	tests := []test{
-		{input: 56.67, want: 134.006},
+		{input: 56.67, want: 134, error: 1e-2},
+                {input: 56.67, want: 134, error: 1e-3},
+                {input: 56.67, want: 134, error: 1e-12},
 	}
-
+/*
 	for _, tc := range tests {
 		got := CelsiusToFahrenheit(tc.input)
 		if !reflect.DeepEqual(tc.want, got) {
 			t.Errorf("expected: %v, got: %v", tc.want, got)
 		}
 	}
+*/  
+      for _, tc := range tests {
+                got := CelsiusToFahrenheit(tc.input)
+                if !withinTolerance(tc.want, got, tc.error) {
+                        t.Errorf("expected: %.18f, got: %.18f", tc.want, got)
+                }
+        }
 }
 
 // Farhrenheit = (Kelvin - 273.15)*(9/5) + 32
